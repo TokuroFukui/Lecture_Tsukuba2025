028223882dde80155bf498da76b876605cbd7aaf
TokuroFukui <tokuro.fukui@artsci.kyushu-u.ac.jp>
2025-02-16 00:19:04 +0900

    Update custom.js

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // New tab when opening external link in navigation menu
-    document.querySelectorAll('.site-nav a[href^="http"]').forEach(link => {
-        if (!link.href.includes(window.location.origin)) {
+    document.querySelectorAll('.site-nav a').forEach(link => {
+        if (link.hostname && link.hostname !== window.location.hostname) {
             link.setAttribute("target", "_blank");
             link.setAttribute("rel", "noopener noreferrer");
         }
